823bce3f58ddef95297d928f4eb75097497460c2
PUNCHH_YOGESH <yogesh@punchh.com>
2018-06-27 18:09:10 +0530

    no message

--- screenshot.js
+++ screenshot.js
@@ -1,3 +1,7 @@
+// Used to take screenshots in detox testing
+// based on the device OS
+// author : Yogesh Thanvi
+// Detox Screenshots 
 const { exec } = require('child-process-async');
 var Platform = require('react-native');
 
@@ -5,12 +9,15 @@ const Screen = (FileName) =>{
   try{
     if(Platform.OS === 'ios')
     {
+      // used to take screenshots for ios device and save to machine
       exec(`xcrun simctl io booted screenshot e2e/screenshots/${FileName++}.png`);
     }
     else
     {
+      //used to take screenshot for android device and save it in android device
       exec(`adb shell screencap -p /sdcard/${FileName++}.png`);
-      exec(`adb pull /sdcard/${FileName++}.png /Users/punchh_yogesh/Noodles/e2e/screenshots`)
+      //Pull the screenshot from device to local directory
+      exec(`adb pull /sdcard/${FileName++}.png /<fullpath of project>/e2e/screenshots`)
     }
   }catch (e)
   {
