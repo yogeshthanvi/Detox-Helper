f9f7798d3fcf3144fb24b015e72d364b686bb7f7
PUNCHH_YOGESH <yogesh@punchh.com>
2018-07-04 16:01:45 +0530

    New changes with make directory and sleep commands

--- screenshot.js
+++ screenshot.js
@@ -1,28 +1,57 @@
 // Used to take screenshots in detox testing
 // based on the device OS
 // author : Yogesh Thanvi
-// Detox Screenshots 
+// Detox Screenshots
 const { exec } = require('child-process-async');
 var Platform = require('react-native');
+var d = new Date();
+const fs = require('fs');
+const path = require('path');
 
 const Screen = (FileName) =>{
   try{
-    if(Platform.OS === 'ios')
+    let dp = '/Users/punchh_yogesh/Noodles/e2e/screenshots/'+d.getTime();
+    if(device.getPlatform() === 'ios')
     {
-      // used to take screenshots for ios device and save to machine
-      exec(`xcrun simctl io booted screenshot e2e/screenshots/${FileName++}.png`);
+      // Create a folder with name as current time
+      mkdirSync(path.resolve(dp));
+      // Take a screeenshot of the iOS Device whhere filename is a string
+      exec(`xcrun simctl io booted screenshot ${dp}/${FileName}.png`);
+      sleep(1000);
     }
     else
     {
-      //used to take screenshot for android device and save it in android device
-      exec(`adb shell screencap -p /sdcard/${FileName++}.png`);
-      //Pull the screenshot from device to local directory
-      exec(`adb pull /sdcard/${FileName++}.png /<fullpath of project>/e2e/screenshots`)
+      mkdirSync(path.resolve(dp));
+      // Take a screenshot of android device and store in device storage
+      exec(`adb shell screencap -p /sdcard/${FileName}.png`);
+      sleep(1000);
+      // Pull the screenshot from device and store in local directory 
+      exec(`adb pull /sdcard/${FileName}.png ${dp}`)
+      sleep(1000);
     }
   }catch (e)
   {
     console.log(e);
   }
 };
+
+// Used to sleep or hold the process
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e10; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+
+// Creates a folder
+const mkdirSync = function (dirPath) {
+  try {
+    fs.mkdirSync(dirPath)
+  } catch (err) {
+    if (err.code !== 'EEXIST') throw err
+  }
+}
 
 export default Screen;
